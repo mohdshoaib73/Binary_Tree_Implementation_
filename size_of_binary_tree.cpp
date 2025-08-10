036f8b13d26352de13b3cc3e4735a9cee7102426
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2025-08-10 16:00:14 +0530

    sumOfBT

--- size_of_binary_tree.cpp
+++ size_of_binary_tree.cpp
@@ -1,35 +1,35 @@
 /*.....................First Method........................... */
-class Solution {
-  public:
-    void SizeOfTree(Node* node,int& count)
-    {
-        if(!node)
-        {
-            return;
-        }
-        count++;
-        SizeOfTree(node->left,count);
-        SizeOfTree(node->right,count);
-    }
-    int getSize(Node* node) {
-        int count=0;
-        SizeOfTree(node,count);
-        return count;
-    }
-};
+// class Solution {
+//   public:
+//     void SizeOfTree(Node* node,int& count)
+//     {
+//         if(!node)
+//         {
+//             return;
+//         }
+//         count++;
+//         SizeOfTree(node->left,count);
+//         SizeOfTree(node->right,count);
+//     }
+//     int getSize(Node* node) {
+//         int count=0;
+//         SizeOfTree(node,count);
+//         return count;
+//     }
+// };
 
 
 
-/*.....................Second Method........................... */
-class Solution {
-  public:
-    int SizeOfTree(Node* node)
-    {
-        if(!node)return 0;
-     return (1+ SizeOfTree(node->left)+SizeOfTree(node->right));
+// /*.....................Second Method........................... */
+// class Solution {
+//   public:
+//     int SizeOfTree(Node* node)
+//     {
+//         if(!node)return 0;
+//      return (1+ SizeOfTree(node->left)+SizeOfTree(node->right));
         
-    }
-    int getSize(Node* node) {
-      return  SizeOfTree(node);
-    }
-};
+//     }
+//     int getSize(Node* node) {
+//       return  SizeOfTree(node);
+//     }
+// };
---
+++ sumOFBinaryTree.cpp
@@ -0,0 +1,9 @@
+/*Sum of binary tree.sum of bt is define as the sum of its node data*/
+class Solution {
+  public:
+    int sumBT(Node* root) {
+        // code here
+        if(!root)return 0;
+        return (root->data+sumBT(root->left)+sumBT(root->right));
+    }
+};
