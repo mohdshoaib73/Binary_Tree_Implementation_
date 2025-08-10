a51efa9a5e84f8bb4a8dafb1d16bedb0adb6a8df
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2025-08-10 16:27:10 +0530

    no. of non leaves node

--- leavesNodeInBT.cpp
+++ leavesNodeInBT.cpp
@@ -19,19 +19,19 @@
 
 
 /*SECOND METHOD*/
-class Solution {
-  public:
-    // Function to count the number of leaf nodes in a binary tree.
-    // void NumberOfLeaves(Node* root,int &leaves)
-    // {
-    //     if(!root)return;
-    //     if(!(root->left) && !(root->right)) leaves++;
-    //     NumberOfLeaves(root->left,leaves);
-    //     NumberOfLeaves(root->right,leaves);
-    // }
-    int countLeaves(Node* root) {
-        if(!root)return 0;
-        if(!(root->left) && !(root->right))return 1;
-        return countLeaves(root->left)+countLeaves(root->right);   
-    }
-};
+// class Solution {
+//   public:
+//     // Function to count the number of leaf nodes in a binary tree.
+//     // void NumberOfLeaves(Node* root,int &leaves)
+//     // {
+//     //     if(!root)return;
+//     //     if(!(root->left) && !(root->right)) leaves++;
+//     //     NumberOfLeaves(root->left,leaves);
+//     //     NumberOfLeaves(root->right,leaves);
+//     // }
+//     int countLeaves(Node* root) {
+//         if(!root)return 0;
+//         if(!(root->left) && !(root->right))return 1;
+//         return countLeaves(root->left)+countLeaves(root->right);   
+//     }
+// };
