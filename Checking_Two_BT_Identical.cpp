971c541d046e069d80d6eec5ad356fe956deba4a
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2025-08-10 19:08:48 +0530

    mirror of a binary tree

--- Checking_Two_BT_Identical.cpp
+++ Checking_Two_BT_Identical.cpp
@@ -1,25 +1,25 @@
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
+// struct Node
+// {
+//     int data;
+//     struct Node* left;
+//     struct Node* right;
 
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};
-*/
+//     Node(int x){
+//         data = x;
+//         left = right = NULL;
+//     }
+// };
+// */
 
-class Solution {
-  public:
-    // Function to check if two trees are identical.
-    bool isIdentical(Node *r1, Node *r2) {
-       if(!r1 && !r2)return 1;//both point null
-       if((r1 && !r2) || (!r1 && r2))return 0;//when one point null and other point a node
-       if(r1->data!=r2->data)return 0;// both node data are not equal or same
+// class Solution {
+//   public:
+//     // Function to check if two trees are identical.
+//     bool isIdentical(Node *r1, Node *r2) {
+//        if(!r1 && !r2)return 1;//both point null
+//        if((r1 && !r2) || (!r1 && r2))return 0;//when one point null and other point a node
+//        if(r1->data!=r2->data)return 0;// both node data are not equal or same
        
        
-      return (isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right));
-    }
-};
+//       return (isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right));
+//     }
+// };
