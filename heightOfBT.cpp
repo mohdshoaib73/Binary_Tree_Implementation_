53e051eb09d1b34e77eb76094acef2a22b3eef70
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2025-08-10 17:17:40 +0530

    max value at each lv

--- heightOfBT.cpp
+++ heightOfBT.cpp
@@ -1,25 +1,25 @@
-/*
-class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
+// /*
+// class Node {
+// public:
+//     int data;
+//     Node* left;
+//     Node* right;
 
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-*/
-class Solution {
-  public:
+//     Node(int val) {
+//         data = val;
+//         left = right = NULL;
+//     }
+// };
+// */
+// class Solution {
+//   public:
   
-    int height(Node* node) {
-        // code here
-       if(!node)
-       return 0;
-       if(!(node->left) && !(node->right))return 0;
-       return (1+max(height(node->left),height(node->right)));
+//     int height(Node* node) {
+//         // code here
+//        if(!node)
+//        return 0;
+//        if(!(node->left) && !(node->right))return 0;
+//        return (1+max(height(node->left),height(node->right)));
         
-    }
-};
+//     }
+// };
---
+++ max_value_at_each_level_of_BT.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> largestValues(Node* root) {
+        // code here
+        vector<int>ans;
+        queue<Node*>q;
+        q.push(root);
+        Node* temp;
+        while(!q.empty())
+        {
+            int n=q.size();//size of each level of tree
+            int maxi=INT_MIN;//storing max value and then init as min value
+            for(int i=0;i<n;i++)
+            {
+               temp=q.front();
+               q.pop();
+               //updating max value
+               maxi=max(maxi,temp->data);
+                //push left child of the node into queue if exists
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                //push right child of the node if exists
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+            //pushing nth level maximum value int the vecotor named ans
+            ans.push_back(maxi);
+        }
+        return ans;
+    }
