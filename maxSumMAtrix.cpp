ddefa0cdb97fb6db856bfe918395e75acb6267b4
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2026-01-05 23:27:14 +0530

    maxSum

---
+++ maxSumMAtrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+
+        long long sum=0;
+
+        int countNeg=0;
+        int smallest=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                sum=sum+abs(matrix[i][j]);
+                if(matrix[i][j]<0)
+                {
+                    countNeg++;
+                }
+                smallest=min(smallest,abs(matrix[i][j]));
+            }
+        }
+
+        if(countNeg%2==0)return sum;
+
+        return sum-(2*smallest);
+    }
+};
