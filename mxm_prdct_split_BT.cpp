7de0f4569386a9db9a8d2843702c3556d8ac705d
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2026-01-07 23:51:28 +0530

    LC1339

---
+++ mxm_prdct_split_BT.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    long M = 1e9+7;
+    long totalSum = 0, maxP = 0;
+    
+    int findTotalSum(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int leftSubtreeSum  = findTotalSum(root->left);
+        int rightSubtreeSum = findTotalSum(root->right);
+        int sum             = root->val + leftSubtreeSum + rightSubtreeSum;
+        
+        maxP = max(maxP, (totalSum - sum) * sum);
+        
+        return sum;
+    }
+    
+    int maxProduct(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        maxP = 0;
+        
+        totalSum = findTotalSum(root);
+        
+        
+        findTotalSum(root);
+        
+        return maxP%M;
+    }
+};
